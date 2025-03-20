dfd5080b5e9aa20e4bb1f6eed70a518879be33d4
Feruzjon Muzaffarov <feruzjonmuzaffarov1209@gmail.com>
2025-03-20 18:05:31 +0500

    Toliq

--- news/models.py
+++ news/models.py
@@ -28,3 +28,18 @@ class Sponsor(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    view = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Contacts"
+        ordering = ['view']
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
--- news/views.py
+++ news/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+import datetime
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from .models import Category, News, Sponsor
+from .forms import ContactForm
+
+def get_date():
+    return datetime.datetime.today()
+
+def get_category():
+    return Category.objects.all()
 
 def index(request):
-    ctg = Category.objects.all()
     news = News.objects.all().order_by('-date')
     news_lasts = News.objects.all().order_by('-date')
     news_uzb = News.objects.filter(category__name="O'zbekiston").order_by('-date')
@@ -11,7 +19,7 @@ def index(request):
     news_fan = News.objects.filter(category__name="Fan-texnika").order_by('-date')
     sponsor = Sponsor.objects.all()
     context = {
-        'ctg': ctg,
+        'ctg': get_category(),
         'news': news,
         'news_lasts': news_lasts[:5],
         'news_uzb': news_uzb,
@@ -19,17 +27,68 @@ def index(request):
         'news_sport': news_sport,
         'news_fan': news_fan,
         'sponsor': sponsor,
+        'date': get_date(),
     }
     return render(request, 'index.html', context)
 
 def about(request):
-    return render(request, '404.html')
+    news = News.objects.all().order_by('-date')
+    news_lasts = News.objects.all().order_by('-date')
+    context = {
+        'ctg': get_category(),
+        'news': news,
+        'news_lasts': news_lasts[:5],
+        'date': get_date(),
+    }
+    return render(request, '404.html', context)
 
-def detail(request):
-    return render(request, 'single_page.html')
+def detail(request, pk):
+    new = News.objects.get(pk=pk)
+    news = News.objects.filter(category__name=new.category).order_by('-date')
+    popular_news = News.objects.filter(category__name=new.category).order_by('views')
+    sponsor = Sponsor.objects.all()
+    new.views += 1
+    new.save()
+    context = {
+        'ctg': get_category(),
+        'new': new,
+        'news': news,
+        'popular_news': popular_news[:4],
+        'date': get_date(),
+        'sponsor': sponsor,
+    }
+    return render(request, 'single_page.html', context)
 
 def contact(request):
-    return render(request, 'contact.html')
+    news = News.objects.all().order_by('-date')
+    popular_news = News.objects.all().order_by('views')
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Xabaringiz muvaffaqiyatli yuborildi!")
+            return redirect('contact')
 
-def error_page(request):
-    return render(request, '404.html')
+    else:
+        form = ContactForm()
+    context = {
+        'ctg': get_category(),
+        'news': news,
+        'popular_news': popular_news[:4],
+        'date': get_date(),
+        'form': form,
+    }
+    return render(request, 'contact.html', context)
+
+
+def category_detail(request, pk):
+    ctg = Category.objects.get(id=pk)
+    active_ctg = Category.objects.get(id=pk)
+    news = News.objects.filter(category__name=ctg.name ).order_by('-date')
+    context = {
+        'ctg': get_category(),
+        'news': news,
+        'date': get_date(),
+        'active_ctg': active_ctg,
+    }
+    return render(request, 'category.html', context)
